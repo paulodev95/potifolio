93180dfe71686237eb5d29cb77569f84a27e0021
paulodev95 <125971494+paulodev95@users.noreply.github.com>
2024-02-02 17:47:28 -0300

    js

--- menu.js
+++ menu.js
@@ -4,3 +4,7 @@ let menu = document.getElementById('menu-mobile')
 btnMenu.addEventListener('click', ()=>{
     menu.classList.add('abrir-menu')
 })
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
